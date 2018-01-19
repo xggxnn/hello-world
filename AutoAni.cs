fd181911704a2d606a835095d7d339d8f01d9bb6
xggxnn <xggxnn@qq.com>
2018-01-19 16:32:49 +0800

    AnimatorControllerAutoCreate
    自动创建AnimatorController并生成prefab

---
+++ AutoAni.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.Animations;
+using System.Collections.Generic;
+
+public class AutoAni : AssetPostprocessor
+{
+    /// <summary>
+    /// 选择文件夹
+    /// </summary>
+    static Object aniObjs;
+    /// <summary>
+    /// 替换的材质
+    /// </summary>
+    static Shader myShader = null;
+    [MenuItem("LYJ/AutoAni", false, 103)]
+    static void AutoCreateAni()
+    {
+        aniObjs = null;
+        aniObjs = Selection.activeObject;
+        if (aniObjs != null)
+        {
+            myShader = Shader.Find("Custom/PlayerDiffuse");
+
+            string fullPath = Path.GetFullPath(AssetDatabase.GetAssetPath(aniObjs));    //"E:/fdsfd/"
+            string path = AssetDatabase.GetAssetPath(aniObjs);                          //"Asset/"
+            if (Directory.Exists(fullPath))                                             // 文件存在
+            {
+                string fbxpath = "";
+                List<AnimationClip> clips = new List<AnimationClip>();
+                DirectoryInfo direction = new DirectoryInfo(fullPath);
+                FileInfo[] files = direction.GetFiles("*",SearchOption.AllDirectories);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string fileName = files[i].Name.ToLower();
+                    if (fileName.EndsWith(".mat"))
+                    {
+                        //修改材质球shader
+                        string fullPath2 = files[i].DirectoryName.Replace('\\', '/');
+                        fullPath2 = fullPath2.Remove(0, fullPath2.LastIndexOf("Assets"));
+                        string realPath = fullPath2 + "/" + files[i].Name.Remove(files[i].Name.LastIndexOf("."));
+                        Material go = AssetDatabase.LoadAssetAtPath(fullPath2 + "/" + files[i].Name, typeof(Material)) as Material;
+                        go.shader = myShader;
+                        Debug.Log("<color=green>材质球修改完毕</color>");
+                    }
+                    else if (fileName.EndsWith(".fbx"))
+                    {
+                        string result = files[i].Name.Remove(files[i].Name.LastIndexOf("."));
+                        if (!result.Contains("_"))
+                        {
+                            fbxpath = files[i].Name;
+                            continue;
+                        }
+                        result = result.Remove(0, result.IndexOf("_") + 1);
+                        result = result.Remove(0, result.IndexOf("_") + 1);
+                        result = result.Remove(0, result.IndexOf("_") + 1);
+                        result = result.Substring(0, 1).ToUpper() + result.Substring(1);
+                        string realPath = path + "/" + files[i].Name;
+                        ModelImporter modelImporter = AssetImporter.GetAtPath(realPath) as ModelImporter;
+                        bool loop = false;
+                        if (result.Equals("Run") || result.Equals("Idle") || result.Equals("Idle1") || result.Equals("Stun") || result.Equals("Idle2") || result.Equals("RunIdle") || result.Equals("RunD") || result.Equals("RunT") || result.Equals("RunU") || result.Equals("RunL") || result.Equals("RunR") || result.Equals("Fix"))
+                        {
+                            loop = true;
+                        }
+                        ModelImporterClipAnimation tempClip = modelImporter.defaultClipAnimations[0];
+                        clipArrayListCreater creater = new clipArrayListCreater();
+                        creater.addClip(result, (int)tempClip.firstFrame, (int)tempClip.lastFrame, loop, tempClip.wrapMode);
+                        modelImporter.clipAnimations = creater.getArray();
+                        clips.Add(AssetDatabase.LoadAssetAtPath(realPath, typeof(AnimationClip)) as AnimationClip);
+                        AssetDatabase.ImportAsset(realPath);
+                    }
+                }
+                CreateAniController(path + "/" + aniObjs.name, clips, path + "/" + fbxpath);
+                AssetDatabase.Refresh();
+                Debug.Log("<color=red>AutoOver</color>");
+                aniObjs = null;
+            }
+        }
+        else
+        {
+            if (EditorUtility.DisplayDialog("", "请选择project面板下fbx文件所在的文件夹", "确定"))
+            {
+                return;
+            }
+        }
+    }
+
+    static void CreateAniController(string path, List<AnimationClip> clips, string fbxPath)
+    {
+        // 创建动画控制器
+        AnimatorController   animatorController = AnimatorController.CreateAnimatorControllerAtPath(path + "Controller.controller");
+        AnimatorControllerLayer layer = null;
+        if (animatorController.layers.Length > 0)
+        {
+            layer = animatorController.layers[0];
+        }
+        AnimatorStateMachine layMachine = layer.stateMachine;
+        Vector3 anyStatePosition = layMachine.anyStatePosition;
+
+        float OFFSET_X = 220;
+        float OFFSET_Y = 60;
+        float ITEM_PER_LINE = 4;
+        float originX = anyStatePosition.x - OFFSET_X * (ITEM_PER_LINE / 2.5f);
+        float originY = -20;
+        float x = originX;
+        float y = originY;
+
+        layMachine.anyStatePosition = new Vector3(-150, -150, 0);
+        layMachine.entryPosition = new Vector3(150, -150, 0);
+
+        UnityEditor.Animations.AnimatorState defAni = null;
+        // 设置默认动作
+        AnimationClip defClip = clips.Find(m => m.name.Equals("Idle"));
+        if (defClip != null)
+        {
+            UnityEditor.Animations.AnimatorState state = layMachine.AddState(defClip.name,new Vector3(0, -100, 0));
+            state.motion = defClip;
+            layMachine.defaultState = state;
+            defAni = state;
+        }
+        else
+        {
+            if (EditorUtility.DisplayDialog("", "未发现idle动作，请检查", "确定"))
+            {
+                /*return*/
+            }
+        }
+
+        foreach (AnimationClip newClip in clips)
+        {
+            if (newClip.name.Equals("Idle"))
+            {
+                continue;
+            }
+            UnityEditor.Animations.AnimatorState state = layMachine.AddState(newClip.name,new Vector3(x, y, 0));
+            state.motion = newClip;
+
+            if (defAni != null)
+            {
+                if (newClip.name.Equals("Hit") || newClip.name.Equals("Attack") || newClip.name.Equals("Stun") || newClip.name.Equals("Skill1") || newClip.name.Equals("Skill2") || newClip.name.Equals("Skill3") || newClip.name.Equals("Skill4") || newClip.name.Equals("Skill5") || newClip.name.Equals("Skill6") || newClip.name.Equals("Skill7") || newClip.name.Equals("Attack1") || newClip.name.Equals("Attack2") || newClip.name.Equals("Attack3") || newClip.name.Equals("Attack4") || newClip.name.Equals("Attack5") || newClip.name.Equals("Attack6"))
+                {
+                    AnimatorStateTransition ast = state.AddTransition(defAni);
+                    ast.hasExitTime = true;
+                    ast.exitTime = 0.99F;
+                    ast.duration = 0.1F;
+                    ast.offset = 0;
+                }
+            }
+            x += OFFSET_X;
+            if (x >= originX + OFFSET_X * ITEM_PER_LINE)
+            {
+                x = originX;
+                y += OFFSET_Y;
+            }
+        }
+        // 创建prefab
+        string name = aniObjs.name;
+        string prefabPath = "Assets/Game/Resources/PrefabUnit/" + aniObjs.name + ".prefab";
+        GameObject go = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath(fbxPath, typeof(GameObject)) as GameObject);
+        if (go == null) return;
+
+
+        go.name = name;
+        Animator animator = go.GetComponent<Animator>();
+        if (animator == null)
+        {
+            animator = go.AddComponent<Animator>();
+        }
+        AnimatorManager aniManager = go.AddComponent<AnimatorManager>();
+        // 攻击点
+        GameObject shotPos = new GameObject();
+        shotPos.name = "ShotPos";
+        shotPos.transform.SetParent(go.transform);
+        shotPos.transform.localEulerAngles = Vector3.zero;
+        shotPos.transform.localPosition = Vector3.zero;
+        shotPos.transform.localScale = Vector3.one;
+        aniManager.ShotPos = shotPos.transform;
+
+        // 攻击点
+        GameObject bloodPos = new GameObject();
+        bloodPos.name = "BloodPos";
+        bloodPos.transform.SetParent(go.transform);
+        bloodPos.transform.localEulerAngles = Vector3.zero;
+        bloodPos.transform.localPosition = new Vector3(0, 4, 0);
+        bloodPos.transform.localScale = Vector3.one;
+        // 受击点
+        GameObject anchorShotBy = new GameObject();
+        anchorShotBy.name = "AnchorShotBy";
+        anchorShotBy.transform.SetParent(go.transform);
+        anchorShotBy.transform.localEulerAngles = Vector3.zero;
+        anchorShotBy.transform.localPosition = new Vector3(0, 4, 0);
+        anchorShotBy.transform.localScale = Vector3.one;
+
+        animator.applyRootMotion = false;
+        animator.runtimeAnimatorController = animatorController;
+        GameObject resObj = PrefabUtility.CreatePrefab(prefabPath, go);
+        GameObject.DestroyImmediate(go);
+        AssetDatabase.SaveAssets();
+
+        if (EditorUtility.DisplayDialog("", "请调整攻击点，血条，受击点位置，prefab位置：" + prefabPath, "确定"))
+        {
+            /*return*/
+            Debug.Log("<color=red>" + prefabPath + "</color>");
+        }
+    }
+}
---
+++ FBXPrefab.cs
@@ -0,0 +1,356 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+using UnityEditor.Animations;
+using Games;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+
+public class FBXPrefab : EditorWindow
+{
+    [MenuItem("LYJ/EditorPrefab")]
+    public static void Open()
+    {
+        EditorWindow.GetWindow<FBXPrefab>();
+    }
+
+    [MenuItem("LYJ/模型测试场景")]
+    static void Go0001()
+    {
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene("Assets/Game/Scenes/Test.unity");
+    }
+
+    [MenuItem("LYJ/战斗测试场景")]
+    static void Go0002()
+    {
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene("Assets/Game/Scenes/War-Debug.unity");
+    }
+
+    [MenuItem("LYJ/主场景入口")]
+    static void Go0003()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Game/Scenes/Launch.unity");
+        //EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        //EditorApplication.OpenScene("Assets/Game/Scenes/Launch.unity");
+    }
+
+    //[MenuItem("LYJ/Select/主场景入口")]
+    //static void Go0004()
+    //{
+    //    EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+    //    EditorSceneManager.OpenScene("Assets/Game/Scenes/Launch.unity");
+    //    //EditorApplication.SaveCurrentSceneIfUserWantsTo();
+    //    //EditorApplication.OpenScene("Assets/Game/Scenes/Launch.unity");
+    //}
+
+    #region 动态生成基本数据
+    private bool PlayOrTown = false;
+    private GameObject Root = null;
+    private string RootName = "root";
+    private GameObject FbxObject = null;
+    private Transform[] mAllChildren;
+    private string Title = "Prefab生成器";
+    #endregion
+
+    //void OnGUI()
+    //{
+    //    EditorGUILayout.Space();
+    //    EditorGUILayout.Space();
+    //    EditorGUILayout.BeginHorizontal();
+    //    GUI.color = Color.red;
+    //    if (Root == null)
+    //    {
+    //        Title = "Prefab生成器";
+    //    }
+    //    else
+    //    {
+    //        if (PlayOrTown)
+    //        {
+    //            Title = "英雄Prefab生成器";
+    //        }
+    //        else
+    //        {
+    //            Title = "机关Prefab生成器";
+    //        }
+    //    }
+    //    GUILayout.Button(Title);
+    //    GUI.color = Color.white;
+    //    EditorGUILayout.EndHorizontal();
+    //    EditorGUILayout.Space();
+    //    EditorGUILayout.Space();
+    //    if (Root == null)
+    //    {
+    //        GUILayout.BeginHorizontal();
+    //        if (GUILayout.Button("生成英雄根节点"))
+    //        {
+    //            PlayOrTown = true;
+    //            Root = new GameObject();
+    //            CharacterController _CC = Root.AddComponent<CharacterController>();
+    //            _CC.center = new Vector3(0, 2, 0);
+    //            _CC.radius = 1.2F;
+    //            _CC.height = 4.2F;
+    //            UnityEngine.AI.NavMeshAgent _NMA = Root.AddComponent<UnityEngine.AI.NavMeshAgent>();
+    //            _NMA.speed = 12;
+    //            _NMA.acceleration = 80;
+    //            _NMA.angularSpeed = 720;
+    //            _NMA.stoppingDistance = 0.01F;
+    //            Object _PFT = Resources.Load("PrefabFx/Other/PlayFowardTip");
+    //            GameObject _PFTObj = Instantiate(_PFT) as GameObject;
+    //            _PFTObj.transform.SetParent(Root.transform);
+    //            _PFTObj.transform.localEulerAngles = Vector3.zero;
+    //            _PFTObj.transform.localPosition = Vector3.zero;
+    //            _PFTObj.transform.localScale = Vector3.one;
+    //            _PFTObj.name = "PlayFowardTip";
+    //            Object _Arr = Resources.Load("PrefabFx/Other/Arrow");
+    //            GameObject _ArrObj = Instantiate(_Arr) as GameObject;
+    //            _ArrObj.transform.SetParent(Root.transform);
+    //            _ArrObj.transform.localEulerAngles = Vector3.zero;
+    //            _ArrObj.transform.localPosition = Vector3.zero;
+    //            _ArrObj.transform.localScale = Vector3.one;
+    //            _ArrObj.name = "Arrow";
+    //            _ArrObj.SetActive(false);
+    //            Root.name = "Root";
+    //        }
+    //        if (GUILayout.Button("生成机关根节点"))
+    //        {
+    //            Root = new GameObject();
+    //            Root.name = "Root";
+    //        }
+    //        GUILayout.EndHorizontal();
+    //    }
+    //    if (Root != null)
+    //    {
+    //        if (PlayOrTown)
+    //        {
+    //            PlayDraw();
+    //        }
+    //    }
+    //}
+
+    //private void    PlayDraw()
+    //{
+    //    GUILayout.BeginHorizontal();
+    //    RootName = GUILayout.TextField(RootName);
+    //    if (GUILayout.Button("<- 修改名称"))
+    //    {
+    //        Root.name = RootName;
+    //    }
+    //    EditorGUILayout.LabelField("Root: " + Root.name);
+    //    GUILayout.EndHorizontal();
+    //    EditorGUILayout.Space();
+    //    EditorGUILayout.Space();
+    //    GUILayout.BeginHorizontal();
+    //    if (FbxObject == null)
+    //    {
+    //        if (GUILayout.Button("选择fbx,点击此按钮加载"))
+    //        {
+    //            Object _o = Selection.activeObject;
+    //            if (_o != null)
+    //            {
+    //                players _players = Root.AddComponent<players>();
+    //                FbxObject = Instantiate(_o) as GameObject;
+    //                FbxObject.transform.SetParent(Root.transform);
+    //                FbxObject.transform.localEulerAngles = Vector3.zero;
+    //                FbxObject.transform.localPosition = Vector3.zero;
+    //                FbxObject.transform.localScale = Vector3.one;
+    //                RootName = _o.name;
+    //                FbxObject.name = _o.name;
+    //                _players.m_AniMg = FbxObject.AddComponent<AnimatorManager>();
+    //                _players.m_charController = Root.GetComponent<CharacterController>();
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (GUILayout.Button("保存结果"))
+    //        {
+    //            CreatePrefabObj(Root, FbxObject.name);
+    //        }
+    //    }
+
+    //    GUILayout.EndHorizontal();
+    //}
+
+    private void CreatePrefabObj(GameObject obj, string prefabName)
+    {
+        string[] str = prefabName.Split('(');
+        string path = str[0] + ".prefab";
+        if (PlayOrTown)
+        {
+            path = "Assets/Game/Resources/PrefabFx/Play/" + path;
+        }
+        else
+        {
+            path = "Assets/Game/Resources/PrefabFx/Organ/" + path;
+        }
+        PrefabUtility.CreatePrefab(path, obj);
+    }
+
+    private void ModifyUIName(string proName)
+    {
+        Selection.activeGameObject.name = proName;
+    }
+
+
+    [MenuItem("LYJ/CloseMeshCollider")]
+    static void CloseMesh()
+    {
+        GameObject _o = (GameObject)Selection.activeObject;
+        if (_o != null)
+        {
+            Transform[] children =   _o.GetComponentsInChildren<Transform>(true);
+            foreach (Transform item in children)
+            {
+                MeshCollider meshCollider = item.GetComponent <MeshCollider>();
+                if (meshCollider != null)
+                {
+                    meshCollider.enabled = false;
+                }
+            }
+        }
+    }
+
+    static Object[] targetObjs;
+    [MenuItem("LYJ/EditSprite &S", false, 100)]
+    static void EditTextureToSprite()
+    {
+        targetObjs = Selection.objects;
+        string _Str = "";
+        if (targetObjs[0])
+        {
+            string path = AssetDatabase.GetAssetPath(targetObjs[0]);
+            _Str = Path.GetFileName(Path.GetDirectoryName(path));
+        }
+        for (int i = 0; i < targetObjs.Length; i++)
+        {
+            if (targetObjs[i] && targetObjs[i] is Texture)
+            {
+                string path = AssetDatabase.GetAssetPath(targetObjs[i]);
+                TextureImporter texture = AssetImporter.GetAtPath(path) as TextureImporter;
+                texture.textureType = TextureImporterType.Sprite;
+                texture.spriteImportMode = SpriteImportMode.Single;
+                texture.spritePixelsPerUnit = 100;
+                texture.spritePackingTag = _Str;
+                texture.mipmapEnabled = false;
+                texture.filterMode = FilterMode.Trilinear;
+                AssetDatabase.ImportAsset(path);
+            }
+        }
+    }
+
+
+    [MenuItem("LYJ/ButtonAni", false, 101)]
+    static void AddButtonAnimator()
+    {
+        GameObject _o = Selection.activeGameObject;
+        if (_o != null)
+        {
+            Transform[] children = _o.transform.GetComponentsInChildren<Transform>(true);
+            foreach (Transform item in children)
+            {
+                Button b = item.GetComponent<Button>();
+                if (b != null)
+                {
+                    if (b.transition != Selectable.Transition.SpriteSwap)
+                    {
+                        b.transition = Selectable.Transition.Animation;
+                        Animator _ani = item.GetComponent<Animator>();
+                        if (_ani == null)
+                        {
+                            _ani = item.gameObject.AddComponent<Animator>();
+                        }
+                        _ani.runtimeAnimatorController = Resources.Load<AnimatorController>("PrefabUI/UI/Button"); ;
+                    }
+                }
+            }
+            Loger.Log("Ok");
+        }
+        else
+        {
+            Loger.LogError("Please Select a GameObject");
+        }
+    }
+
+    static Object aniObjs;
+    //[MenuItem("LYJ/ChangeAni", false, 102)]
+    //static void ChangeAnimator()
+    //{
+    //    aniObjs = Selection.activeObject;
+    //    string fullPath = "";
+    //    if (aniObjs)
+    //    {
+    //        fullPath = Path.GetFullPath(AssetDatabase.GetAssetPath(aniObjs));
+    //        string path = AssetDatabase.GetAssetPath(aniObjs);
+    //        if (Directory.Exists(fullPath))
+    //        {
+    //            DirectoryInfo direction = new DirectoryInfo(fullPath);
+    //            FileInfo[] files = direction.GetFiles("*",SearchOption.TopDirectoryOnly);
+    //            for (int i = 0; i < files.Length; i++)
+    //            {
+    //                if (files[i].Name.EndsWith(".FBX") || files[i].Name.EndsWith(".fbx"))
+    //                {
+    //                    string result = files[i].Name.Remove(files[i].Name.LastIndexOf("."));
+    //                    if (!result.Contains("_"))
+    //                    {
+    //                        continue;
+    //                    }
+    //                    result = result.Remove(0, result.IndexOf("_") + 1);
+    //                    result = result.Remove(0, result.IndexOf("_") + 1);
+    //                    result = result.Remove(0, result.IndexOf("_") + 1);
+    //                    result = result.Substring(0, 1).ToUpper() + result.Substring(1);
+    //                    string realPath = path + "/" + files[i].Name;
+    //                    ModelImporter modelImporter = AssetImporter.GetAtPath(realPath) as ModelImporter;
+    //                    bool loop = false;
+    //                    if (result.Equals("Run") || result.Equals("Idle") || result.Equals("Stun") || result.Equals("Idle2") || result.Equals("RunIdle") || result.Equals("RunD") || result.Equals("RundU") || result.Equals("RunL") || result.Equals("RunR"))
+    //                    {
+    //                        loop = true;
+    //                    }
+    //                    LLL.LL(files[i].Name + "    修改为：   " + result + (loop ? "   循环播放" : "   一次性"));
+    //                    ModelImporterClipAnimation tempClip = modelImporter.defaultClipAnimations[0];
+    //                    clipArrayListCreater creater = new clipArrayListCreater();
+    //                    creater.addClip(result, (int)tempClip.firstFrame, (int)tempClip.lastFrame, loop, tempClip.wrapMode);
+    //                    modelImporter.clipAnimations = creater.getArray();
+
+    //                    AssetDatabase.ImportAsset(realPath);
+    //                }
+    //            }
+    //            LLL.LL("请在死亡动作添加event 555","red");
+    //            AssetDatabase.Refresh();
+    //        }
+    //    }
+    //}
+}
+
+public class clipArrayListCreater
+{
+    private List<ModelImporterClipAnimation> clipList = new List<ModelImporterClipAnimation>();
+    public void addClip(string name, int firstFrame, int lastFrame, bool loop, WrapMode wrapMode)
+    {
+        ModelImporterClipAnimation tempClip = new ModelImporterClipAnimation();
+        tempClip.name = name;
+        tempClip.firstFrame = firstFrame;
+        tempClip.lastFrame = lastFrame;
+        tempClip.loopTime = loop;
+        tempClip.wrapMode = wrapMode;
+        //if (name.Equals("Death"))
+        //{
+        //    AnimationEvent evt = new AnimationEvent();
+        //    evt.time = tempClip.lastFrame * 0.85F;
+        //    evt.functionName = "NewEvent";
+        //    evt.intParameter = 555;
+        //    tempClip.events = new AnimationEvent[1];
+        //    tempClip.events[0] = evt;
+        //}
+        clipList.Add(tempClip);
+    }
+
+    public ModelImporterClipAnimation[] getArray()
+    {
+        return clipList.ToArray();
+    }
+}
